f10206ba9ad59a5ad632c4987ea97dc5021f1871
rlanigan <rlanigan@umich.edu>
2019-07-03 14:24:03 -0700

    Update sportsRadar.js

--- sportsRadar/sportsRadar.js
+++ sportsRadar/sportsRadar.js
@@ -6,7 +6,8 @@
             {id: "week_id", dataType: tableau.dataTypeEnum.string},
             {id: "week_num", dataType: tableau.dataTypeEnum.float},
             {id: "game_id", dataType: tableau.dataTypeEnum.string},
-            {id: "home_team", dataType: tableau.dataTypeEnum.string}
+            {id: "home_team", dataType: tableau.dataTypeEnum.string},
+            {id: "away_team", dataType: tableau.dataTypeEnum.string}
         ];
 
         var tableSchema = {
@@ -27,18 +28,22 @@
                 resp."name ov array you want to iterate on"
                 If there are sub arrays (particular games within a particular week) then you will need a nested for loop
             */
-            var feat = resp.weeks,
-                tableData = [];
+            var feat = resp.weeks, tableData = [];
 
             // Iterate over the JSON object
-            for (var i = 0, len = feat.length; i < len; i++) {
-                for (var n = 0, len = feat[i].games.length; n < len; n++) {
+            for (var i = 0, week_count = feat.length; i < week_count; i++) {
+                for (var n = 0, game_count = feat[i].games.length; n < game_count; n++) {
+                    console.log("week_id", feat[i].id, "week_num", feat[i].number);
+                    console.log("     ", "game_id", feat[i].games[n].id, "home_team", feat[i].games[n].home, "away_team", feat[i].games[n].away);
+                    /*
                     tableData.push({
                         "week_id": feat[i].id,
                         "week_num": feat[i].number,
                         "game_id": feat[i].games[n].id,
-                        "home_team": feat[i].games[n].home
+                        "home_team": feat[i].games[n].home,
+                        "away_team": feat[i].games[n].home
                     });
+                    */
                 }
             }
 
@@ -47,6 +52,7 @@
         });
     };
 
+    
     tableau.registerConnector(myConnector);
     $(document).ready(function () {
         $("#submitButton").click(function () {
