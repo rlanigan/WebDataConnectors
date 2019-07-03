30705472bb0bc24230de405542c0ce0c4f9cb0ff
rlanigan <rlanigan@umich.edu>
2019-07-02 19:02:05 -0700

    yup

--- sportsRadar/sportsRadar.js
+++ sportsRadar/sportsRadar.js
@@ -3,14 +3,15 @@
 
     myConnector.getSchema = function (schemaCallback) {
         var cols = [
-            {id: "id",
-                dataType: tableau.dataTypeEnum.string
-            }
+            {id: "week_id", dataType: tableau.dataTypeEnum.string},
+            {id: "week_num", dataType: tableau.dataTypeEnum.float},
+            {id: "game_id", dataType: tableau.dataTypeEnum.string},
+            {id: "home_team", dataType: tableau.dataTypeEnum.string}
         ];
 
         var tableSchema = {
-            id: "earthquakeFeed",
-            alias: "Earthquakes with magnitude greater than 4.5 in the last seven days",
+            id: "Sports Feed",
+            alias: "Something Something Sports ",
             columns: cols
         };
 
@@ -21,17 +22,24 @@
 
     myConnector.getData = function(table, doneCallback) {
         $.getJSON("https://api.sportradar.us/ncaafb-b1/2018/REG/schedule.json?api_key=usevcuzc3r7tcbsezpsn2jaa", function(resp) {
-            var feat = resp.features,
+            /*
+                This is where you will change what you are targeting, and what you are iterating over
+                resp."name ov array you want to iterate on"
+                If there are sub arrays (particular games within a particular week) then you will need a nested for loop
+            */
+            var feat = resp.weeks,
                 tableData = [];
 
             // Iterate over the JSON object
             for (var i = 0, len = feat.length; i < len; i++) {
-                tableData.push({
-                    "id": feat[i].id,
-                    "mag": feat[i].properties.mag,
-                    "title": feat[i].properties.title,
-                    "location": feat[i].geometry
-                });
+                for (var j = 0, len = feat[i].games.length; j < len; j++) {
+                    tableData.push({
+                        "week_id": feat[i].id,
+                        "week_num": feat[i].number,
+                        "game_id": feat[i].games[j].id,
+                        "home_team": feat[i].games[j].home
+                    });
+                }
             }
 
             table.appendRows(tableData);
